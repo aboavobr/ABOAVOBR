f479d8180c7e3aa81b999e20df21c4cdba5bbe9f
huserben <benj.huser@gmail.com>
2019-02-21 20:29:14 +0100

    Add Debug controller, remove sample code

---
+++ RaspberryPi/aboavobr-raspberrypi/Controllers/DebugController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aboavobr_raspberrypi.Controllers
+{
+   [Route("api/[controller]")]
+   [ApiController]
+   public class DebugController : ControllerBase
+   {
+      // GET api/debug
+      [HttpGet]
+      public ActionResult<IEnumerable<string>> Get()
+      {
+         return new string[] { "It's alive" };
+      }
+   }
+}
