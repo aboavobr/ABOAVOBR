14f75e16fabbf7301a2f0ba1944fd07c6889c21f
huserben <benj.huser@gmail.com>
2019-02-28 22:04:17 +0100

    Build docker image based on resin/armv7hf-debian-qemu

--- Arduino/src/sketch_aboavobr.ino
+++ Arduino/src/sketch_aboavobr.ino
@@ -1,9 +1,29 @@
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  while(!Serial);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (Serial.available())
+  {
+	  int state = Serial.parseInt();
+	  
+	  if (state == 0)
+	  {
+		 digitalWrite(LED_BUILTIN, LOW);
+	  }
+	  else if(state == 1)
+	  {
+		  digitalWrite(LED_BUILTIN, HIGH);
+	  }
+	  else
+	  {
+		Serial.write("nope");
+	  }
+  }
+  
+  delay(1000);
 }
