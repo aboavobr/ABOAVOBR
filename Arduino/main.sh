9be3195f874a3c77c9720523a15b011b1c8ddf46
Kim Lillås <kim.lillas@gmail.com>
2019-03-03 22:50:03 +0200

    Modify the arduino main.sh file to actually WORK

--- Arduino/main.sh
+++ Arduino/main.sh
@@ -1,47 +1,26 @@
-if [ -z "$1" ]
-then
-    bash
-elif [ "$1" = "init" ]
-then
-    if [ ! -z "$2" ]
-    then
-        mkdir "$2"
-        cd "$2"
-    fi
-    ino init
-    git init
-    # Git ignore
-    echo '.build' >> .gitignore
-    # gitlab-ci
-    cat > .gitlab-ci.yml << EOF
-image: strm/dev-arduino
+# First Input is Board Model, default is uno (see ino build --help)
+# For the full list of board names refer to ino build --help
+# See here for more help: http://inotool.org/quickstart#tweaking-parameters
+BOARD_MODEL="uno"
+PORT=""
 
-build:
-  stage: build
-  script: 
-    - ino build
-    - cp .build/uno/firmware.hex .
-  artifacts:
-      name: 'firmaware-${CI_BUILD_REF_NAME}-${CI_BUILD_ID}'
-      paths:
-          - firmware.hex
-EOF
-    git add *
-    git add .gitignore
-    git add .gitlab-ci.yml
-elif [ "$1" = "build" ]
-then
-    ino build
-elif [ "$1" = "upload" ]
-then
-    ino upload
-elif [ "$1" = "serial" ]
-then
-    ino serial
-elif [ "$1" = "sh" ]
-then
-    bash
+if [ -z "$1" ]
+  then
+    echo "No board model specified - will use uno"
 else
-    echo '[-] Command not found '$1
-    exit -1
+  echo "Using specified board model: " $1
+  BOARD_MODEL="$1"
 fi
+
+if [ -z "$2" ]
+  then
+    echo "No serial port specified - will use default"
+else
+  echo "Using specified Port: " $2
+  PORT="-p $2"
+fi
+
+
+
+ino build -m $BOARD_MODEL $PORT
+ino upload -m $BOARD_MODEL $PORT
