a9acb92796b6ff728620f84ce9c5666404d1c252
huserben <benj.huser@gmail.com>
2019-03-21 19:40:28 +0100

    Add Swagger API Documentation
    Adds Swashbuckle package and exposes swagger REST API documentation under <ip>:<port>/swagger

--- RaspberryPi/aboavobr.raspberrypi/Controllers/AppController.cs
+++ RaspberryPi/aboavobr.raspberrypi/Controllers/AppController.cs
@@ -3,27 +3,31 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace aboavobr.raspberrypi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AppController : ControllerBase
-    {
-        private readonly ISerialCommunicationService serialCommunicaitonService;
+   [Route("api/[controller]")]
+   [ApiController]
+   public class AppController : ControllerBase
+   {
+      private readonly ISerialCommunicationService serialCommunicaitonService;
 
-        public AppController(ISerialCommunicationService serialCommunicaitonService)
-        {
-            this.serialCommunicaitonService = serialCommunicaitonService;
-        }
+      public AppController(ISerialCommunicationService serialCommunicaitonService)
+      {
+         this.serialCommunicaitonService = serialCommunicaitonService;
+      }
 
-        [HttpGet("heartbeat")]
-        public ActionResult GetHeartbeat()
-        {
-            return Ok();
-        }
+      [HttpGet("heartbeat")]
+      public ActionResult GetHeartbeat()
+      {
+         return Ok();
+      }
 
-        [HttpGet("battery")]
-        public ActionResult<int> GetBatteryLife()
-        {
-            return new ActionResult<int>(serialCommunicaitonService.GetBatteryLife());
-        }
-    }
+      /// <summary>
+      /// Gets the battery life in percentage.
+      /// </summary>
+      /// <returns>The battery state in percentage</returns>
+      [HttpGet("battery")]
+      public ActionResult<int> GetBatteryLife()
+      {
+         return new ActionResult<int>(serialCommunicaitonService.GetBatteryLife());
+      }
+   }
 }
--- RaspberryPi/aboavobr.raspberrypi/Startup.cs
+++ RaspberryPi/aboavobr.raspberrypi/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace aboavobr.raspberrypi
 {
@@ -21,6 +22,16 @@ namespace aboavobr.raspberrypi
       public void ConfigureServices(IServiceCollection services)
       {
          services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+         services.AddSwaggerGen(c =>
+         {
+            c.SwaggerDoc(
+               "v1",
+               new Info
+               {
+                  Title = "ABOAVOBR Rest API",
+                  Version = "v1"                  
+               });
+         });
 
          services.AddSingleton<ISerialPortFactory, SerialPortFactory>();
          services.AddSingleton<ISerialCommunicationService, SerialCommunicationService>();
@@ -49,6 +60,9 @@ namespace aboavobr.raspberrypi
          {
             app.UseHsts();
          }
+
+         app.UseSwagger();
+         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ABOAVOBR Rest API v1"));
 
          app.UseMvc();
       }
